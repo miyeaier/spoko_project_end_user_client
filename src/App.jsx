b9786a8a63d57982f73c82ea8c8417a96a1fcbe1
miyeaier <miyesier@gmail.com>
2022-05-04 00:18:23 +0200

    add footer to app.jsx and format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 import Nav_bar from "./Components/Nav_bar.jsx";
-import Header_image from './Components/Header_image.jsx';
-
+import Header_image from "./Components/Header_image.jsx";
+import Footer from "./Components/Footer.jsx";
+Footer;
 const App = () => {
   return (
     <>
       <Header_image title="NewsChannel" subTitle="Header subtitle" />
       <Nav_bar />
       <div data-cy="name">Spoko</div>
+      <Footer />
     </>
   );
-}
+};
 
-export default App
+export default App;
