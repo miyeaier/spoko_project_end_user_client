054ba5c6579d61b87dbdb637e270754c00cf551f
shagufta <shagufta.batool8@gmail.com>
2022-05-10 09:02:47 +0200

    removes comments

--- src/Components/ArticleCategory.jsx
+++ src/Components/ArticleCategory.jsx
@@ -7,8 +7,6 @@ import "./Styling/Articles.css";
 const ArticleCategory = () => {
   const { category } = useParams();
   const { articles } = useSelector((state) => state);
-  //const { userCountry } = useSelector((state) => state);
-  //const country = userCountry;
 
   const articlesList = articles[category]?.map((article) => {
     return (
@@ -61,33 +59,6 @@ const ArticleCategory = () => {
       </Grid>
     </Container>
   );
-  //   return (
-  //     <li key={article.id} style={{ listStyleType: "none" }}>
-  //       <Card>
-  //         <Link to={`/article/${article.id}`}>
-  //           <div>
-  //             <h1 data-cy="article-title">{article.title}</h1>{" "}
-  //           </div>
-  //           <img
-  //             src={article.image}
-  //             alt=""
-  //             style={{ height: 200 + "px", width: "auto" }}
-  //           />
-
-  //         </Link>
-  //       </Card>
-  //     </li>
-  //   );
-  // });
-
-  // return (
-  //   <Container text>
-  //     <div data-cy="category_header">
-  //       The latest {category} news.
-  //     </div>
-  //     <ul data-cy="articles-list">{articlesList}</ul>
-  //   </Container>
-  // );
 };
 
 export default ArticleCategory;
